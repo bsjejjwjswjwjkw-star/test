8a58f616385467da1390915738fa6c5c7a8e7b1a
bsjejjwjswjwjkw-star <bsjejjwjswjwjkw@gmail.com>
2026-04-12 16:25:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const subText = document.getElementById("sub-text");
 
+const description = "На цьому сайті ви знайдете корисні статті про виховання, психологію дітей, здоров’я та розвиток. Матеріали створені для турботливих батьків.";
+
+function startSubText() {
+    subText.classList.add("show");
+
+    let index = 0;
+    function typeDescription() {
+        if (index < description.length) {
+            subText.textContent += description.charAt(index);
+            index++;
+            setTimeout(typeDescription, 35);
+        }
+    }
+
+    typeDescription();
+}
+
+window.onload = function() {
+    setTimeout(startSubText, 2300);
+};
