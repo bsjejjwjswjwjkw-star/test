d28a6806d698aadf7b83ba853ce6b506c06ac68c
bsjejjwjswjwjkw-star <bsjejjwjswjwjkw@gmail.com>
2026-04-15 22:35:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function startSubText() {
 window.onload = function() {
     setTimeout(startSubText, 2300);
 };
+window.addEventListener("DOMContentLoaded", () => {
+    startSubText();
+
+    const categories = document.querySelectorAll(".category");
+    const articles = document.querySelectorAll(".article");
+
+    categories.forEach(category => {
+        category.addEventListener("click", () => {
+            const selected = category.getAttribute("data-category");
+
+            articles.forEach(article => {
+                const articleCategory = article.getAttribute("data-category");
+
+                article.style.display =
+                    selected === "all" || selected === articleCategory
+                    ? "block"
+                    : "none";
+            });
+        });
+    });
+});
